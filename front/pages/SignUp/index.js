9603a696c6211e48460d59d8959fdea3d60bcbf9
jungwoole91 <jungwoole91@gmail.com>
2021-09-24 02:39:30 +0900

    2-1) 회원가입 페이지 만들기

--- front/pages/SignUp/index.js
+++ front/pages/SignUp/index.js
@@ -22,15 +22,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importStar(require("react"));
 var styles_1 = require("./styles");
 var SignUp = function () {
-    var email = (0, react_1.useState)('')[0];
-    var nickname = (0, react_1.useState)('')[0];
-    var password = (0, react_1.useState)('')[0];
-    var passwordCheck = (0, react_1.useState)('')[0];
-    var onChangeEmail = (0, react_1.useCallback)(function () { }, []);
-    var onChangeNickname = (0, react_1.useCallback)(function () { }, []);
-    var onChangePassword = (0, react_1.useCallback)(function () { }, []);
-    var onChangePasswordCheck = (0, react_1.useCallback)(function () { }, []);
-    var onSubmit = (0, react_1.useCallback)(function () { }, []);
+    var _a = (0, react_1.useState)(''), email = _a[0], setEmail = _a[1];
+    var _b = (0, react_1.useState)(''), nickname = _b[0], setNickname = _b[1];
+    var _c = (0, react_1.useState)(''), password = _c[0], setPassword = _c[1];
+    var _d = (0, react_1.useState)(''), passwordCheck = _d[0], setPasswordCheck = _d[1];
+    var _e = (0, react_1.useState)(false), mismatchError = _e[0], setMismatchError = _e[1];
+    var onChangeEmail = (0, react_1.useCallback)(function (e) {
+        setEmail(e.target.value);
+    }, []);
+    var onChangeNickname = (0, react_1.useCallback)(function (e) {
+        setNickname(e.target.value);
+    }, []);
+    var onChangePassword = (0, react_1.useCallback)(function (e) {
+        setPassword(e.target.value);
+        setMismatchError(e.target.value !== passwordCheck);
+    }, [passwordCheck]);
+    var onChangePasswordCheck = (0, react_1.useCallback)(function (e) {
+        setPasswordCheck(e.target.value);
+        setMismatchError(e.target.value !== password);
+    }, [password]);
+    var onSubmit = (0, react_1.useCallback)(function (e) {
+        e.preventDefault();
+        console.log(email, nickname, password, passwordCheck);
+        if (!mismatchError) {
+            console.log('서버로 회원가입하기');
+        }
+    }, [email, nickname, password, passwordCheck]);
+    console.log(mismatchError);
     return (react_1.default.createElement("div", { id: "container" },
         react_1.default.createElement(styles_1.Header, null, "Sleact"),
         react_1.default.createElement(styles_1.Form, { onSubmit: onSubmit },
@@ -49,7 +67,9 @@ var SignUp = function () {
             react_1.default.createElement(styles_1.Label, { id: "password-check-label" },
                 react_1.default.createElement("span", null, "\uBE44\uBC00\uBC88\uD638 \uD655\uC778"),
                 react_1.default.createElement("div", null,
-                    react_1.default.createElement(styles_1.Input, { type: "password", id: "password-check", name: "password-check", value: passwordCheck, onChange: onChangePasswordCheck }))),
+                    react_1.default.createElement(styles_1.Input, { type: "password", id: "password-check", name: "password-check", value: passwordCheck, onChange: onChangePasswordCheck })),
+                mismatchError && react_1.default.createElement(styles_1.Error, null, "\uBE44\uBC00\uBC88\uD638\uAC00 \uC77C\uCE58\uD558\uC9C0 \uC54A\uC2B5\uB2C8\uB2E4."),
+                !nickname && react_1.default.createElement(styles_1.Error, null, "\uB2C9\uB124\uC784\uC744 \uC785\uB825\uD574\uC8FC\uC138\uC694.")),
             react_1.default.createElement(styles_1.Button, { type: "submit" }, "\uD68C\uC6D0\uAC00\uC785")),
         react_1.default.createElement(styles_1.LinkContainer, null,
             "\uC774\uBBF8 \uD68C\uC6D0\uC774\uC2E0\uAC00\uC694?\u00A0",
